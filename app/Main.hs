660f9fabba1bf58034bdf0e7ebe43f68dd9a494f
Eric Hardy <eric.allen.hardy@gmail.coml>
2019-09-29 23:00:26 -0400

    starting Eval

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import           Data.Maybe               (listToMaybe)
+import           SchemeInterpreter.Eval   (eval, functionEnv)
+import           SchemeInterpreter.Parser (readExpr)
+import           System.Environment       (getArgs)
+
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case listToMaybe args of
+    Just file ->
+      let result = eval functionEnv =<< readExpr file
+       in print (either show show result)
+    Nothing -> print "Please give a file to eval"
